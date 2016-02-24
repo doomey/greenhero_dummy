28da07ceb527b98c37b58ef19455d663e76edec6
HAEBEOM <haebeompark@naver.com>
2016-02-24 17:01:36 +0900

    photo, multimedia 수정했습니다.

--- routes/multimedia.js
+++ routes/multimedia.js
@@ -1,11 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+var fs = require('fs');
+var mime = require('mime');
 
 router.get('/:multimedia_id', function(req, res, next) {
-    var multimediaId = req.params;
-    res.json({
-        "result": {
-            "message" : "동영상을 가져왔습니다."
+    var fileName = req.params.multimedia_id;
+    var pathObj = path.parse(__dirname);
+    var filePath = path.join(pathObj.dir, '/public', fileName);
+
+    fs.stat(filePath, function(err, stats){
+        if(err){
+            var err = {
+                "code" : "err026",
+                "message" : "동영상을 불러오는데 실패하였습니다."
+            }
+            next(err);
+        } else {
+            var reader = fs.createReadStream(filePath);
+            var mimeType = mime.lookup(fileName);
+
+            res.writeHead(200,{'content-type' : mimeType});
+            reader.pipe(res);
         }
     });
 });
--- routes/photo.js
+++ routes/photo.js
@@ -1,12 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+var fs = require('fs');
+var mime = require('mime');
 
 router.get('/:photo_id', function(req, res, next) {
-    var photoId = req.params;
 
-    res.json({
-        "result": {
-            "message" : "사진을 가져왔습니다."
+    var fileName = req.params.photo_id;
+    var pathObj = path.parse(__dirname);
+    var filePath = path.join(pathObj.dir, '/public', fileName);
+
+    fs.stat(filePath, function(err, stats){
+        if(err){
+            var err = {
+                "code" : "err025",
+                "message" : "사진 불러오기에 실패하였습니다."
+            }
+            next(err);
+        } else {
+            var reader = fs.createReadStream(filePath);
+            var mimeType = mime.lookup(fileName);
+
+            res.writeHead(200,{'content-type' : mimeType});
+            reader.pipe(res);
         }
     });
 });

cb721e22e75d437d5cb654d92f597b106be20db5
kirsch <kirsch2u@naver.com>
2016-02-26 09:50:08 +0900

    더미 복구

--- routes/receipt.js
+++ routes/receipt.js
@@ -1,65 +1,27 @@
 var express = require('express');
 var router = express.Router();
-var async = require('async');
+
 router.get('/', function(req, res, next) {
     if(req.secure) {
-        var page = parseInt(req.query.page);
-        page = isNaN(page)? 1 : page;
-        page = (page<1)? 1 : page;
-
-        var limit = 2;
-        var offset = limit*(page-1);
-        //1. connection
-        //orders, orderdetails join
-
-        function getConnection(callback) {
-            pool.getConnection(function(err, connection) {
-                if(err) {
-                    callback(err);
-                } else {
-                    callback(null, connection);
-                }
-            });
-        }
-
-        function selectOrders(connection, callback) {
-            var select = "select o.id as id, g.name as name, g.picture as picture, g.price as price, od.quantity as quantity, (g.price * od.quantity) as iprice "+
-                         "from greendb.orders o join greendb.orderdetails od on (o.id = od.order_id) "+
-                         "join greendb.greenitems g on (od.greenitems_id = g.id) "+
-                         "where iparty_id = ? limit ? offset ?";
-            connection.query(select, [1, limit, offset], function(err, results) {
-                connection.release();
-                if(err) {
-                    callback(err);
-                } else {
-                    var message = {
-                        "result" : {
-                            "page" : page,
-                            "itemsPerPage" : limit,
-                            "items" : []
-                        }
-                    };
-                    console.log(message);
-                    results.forEach(function(item) {
-                        message.result.items.push({
-                            "id" : item.id,
-                            "name" : item.name,
-                            "picture" : item.picture,
-                            "price" : item.price,
-                            "quantity" : item.quantity,
-                            "iPrice" : item.iprice
-                        });
-                    });
-                    console.log(message);
-                    callback(null, message);
-                }
-            });
-        }
-        async.waterfall([getConnection, selectOrders], function(err, message) {
-            if(err) {
-                next(err);
-            } else {
-                res.json(message);
+        res.json({
+            "result" : {
+                "page" : 1,
+                "itemsPerPage" : 2,
+                "items" : [{
+                    "id" : 1,
+                    "name" : "하기스",
+                    "picture" : "/photos/xxxxxxx.jpg",
+                    "price" : 1000,
+                    "quantity" : 1,
+                    "iPrice" : 1000,
+                }, {
+                    "id" : 2,
+                    "name" : "팬티",
+                    "picture" : "/photos/xxxxxxx.jpg",
+                    "price" : 500,
+                    "quantity" : 2,
+                    "iPrice" : 1000
+                }]
             }
         });
     } else {

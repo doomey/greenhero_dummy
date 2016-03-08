e641fc9b7405a6eb4148c3cc577d013a2fddc01d
HAEBEOM <haebeompark@naver.com>
2016-03-08 17:31:33 +0900

    faq : 2, accessterms : 3

--- routes/accessterms.js
+++ routes/accessterms.js
@@ -9,13 +9,13 @@ router.get('/', function(req, res, next){
                 "list" : [
                     {
                         "id" : 1,
-                        "type": 2,
+                        "type": 3,
                         "title": "서비스약관",
                         "date": "2016-02-01 15:00:00"
                     },
                     {
                         "id" : 2,
-                        "type": 2,
+                        "type": 3,
                         "title": "이용약관",
                         "date": "2016-02-02 15:00:00"
                     }
@@ -33,14 +33,14 @@ router.get('/:accesstermId', function(req, res, next){
             "list" : [
                 {
                     "id" : 1,
-                    "type": 2,
+                    "type": 3,
                     "title": "서비스약관",
                     "date": "2016-02-01 15:00:00",
                     "body": "서비스약관의 내용이 들어간다."
                 },
                 {
                     "id" : 2,
-                    "type": 2,
+                    "type": 3,
                     "title": "이용약관",
                     "date": "2016-02-02 15:00:00",
                     "body": "이용약관의 내용이 들어간다."
